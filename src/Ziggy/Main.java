967af8af969b14b62b408688ff737347832706a2
sheikshahith18 <115651220+sheikshahith18@users.noreply.github.com>
2022-11-17 12:56:57 +0530

    Add files via upload

---
+++ src/Ziggy/Main.java
@@ -0,0 +1,74 @@
+package Ziggy;
+
+import hotel.HotelMain;
+
+import customer.CustomerMain;
+
+import utility.Init;
+import utility.Input;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("\t\t\t\t\t----------Welcome to Ziggy----------\n");
+
+		Init.initializeHotel();
+		Init.initializeCustomer();
+
+//				  HOTEL LOGIN : 
+//				  username : hotel@gmail.com 
+//				  password : Qwerty@123 
+
+//		  		  CUSTOMER LOGIN : 
+//		  		  username : customer1@gmail.com , customer2@gmail.com 
+//		  		  password : Qwerty@123
+
+//				  UPI ID   : 1234
+//				  UPI PIN  : 1234
+
+		int choice;
+		do {
+
+			homePage();
+			choice = Input.getInteger(false);
+
+			switch (choice) {
+			case 1: {
+
+				HotelMain hm = new HotelMain();
+				hm.hotelMainMenu();
+				break;
+			}
+			case 2: {
+
+				CustomerMain cm = new CustomerMain();
+				cm.customerMainMenu();
+				break;
+			}
+			case 3: {
+				break;
+			}
+			default: {
+
+				System.out.println("Invalid Option...Try again....");
+			}
+			}
+
+		} while (choice != 3);
+
+	}
+
+	public static void homePage() {
+		System.out.println("\t\t\t ---------------------------------------------");
+		System.out.println("\t\t\t|                 MAIN MENU                   |");
+		System.out.println("\t\t\t ---------------------------------------------");
+
+		System.out.println("\n1.Hotel");
+		System.out.println("2.Customer");
+		System.out.println("3.Exit\n");
+		System.out.print("Enter Your Option : ");
+	}
+
+}
